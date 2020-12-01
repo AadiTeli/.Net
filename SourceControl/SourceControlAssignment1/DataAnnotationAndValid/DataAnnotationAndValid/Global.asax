08d4a145fe4d76c9fc52838d536e36236681b44c
mrat17 <aaditeli2@gmail.com>
2020-12-01 21:23:37 +0530

    The repository is mainly focused on the pre-joining training as well as after joining training in the Gateway group of companies.

---
+++ SourceControl/SourceControlAssignment1/DataAnnotationAndValid/DataAnnotationAndValid/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DataAnnotationAndValid.MvcApplication" Language="C#" %>
---
+++ SourceControl/SourceControlAssignment1/DataAnnotationAndValid/DataAnnotationAndValid/Models/Registration.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System;
+using System.Reflection.Metadata;
+
+namespace DataAnnotationAndValid.Models
+{
+    [Bind(Exclude = "Id")]
+    public class RegistrationModel
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First Name is Required")]
+        [StringLength(20, MinimumLength = 3)]
+        public string Names { get; set; }
+
+        [Required(ErrorMessage = "Email ID is Required")]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(50)]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Incorrect Email Format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Confirm Email is mandatory")]
+        [DataType(DataType.EmailAddress)]
+        [System.ComponentModel.DataAnnotations.Compare("Email", ErrorMessage = "Email Not Matched")]
+        public string ConfirmEmail { get; set; }
+
+       
+
+        [Required(ErrorMessage = "Age is Required")]
+        //[Range(18,50, ErrorMessage ="Age Must Be Btwn 18-50")]
+        [FormValidation.CustomAttribute.MinAge(18)]
+        public int Age { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{2})[-. ]?([0-9]{4})[-. ]?([0-9]{3})[-. ]?([0-9]{3})$", ErrorMessage = "Not a valid Phone number")]
+        public string Mobile { get; set; }
+    }
+}
